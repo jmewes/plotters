2cad8a4e3dfd3b65886eefcd4ddac127e537e9db
Jan Mewes <jan.mewes@experimental-software.com>
2022-01-02 12:52:48 +0100

    Add mermaid script

---
+++ bin/mermaid.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+FORMAT="png"
+MERMAID_VERSION=8.13.5 # https://github.com/mermaid-js/mermaid-cli/tags
+
+while getopts "f: w h" flag; do
+case "$flag" in
+    h) HELP='true';;
+    w) WATCH='true';;
+    f) FORMAT=$OPTARG;;
+esac
+done
+
+DIAGRAM=${@:$OPTIND:1}
+
+if [[ $HELP == 'true' || -z "$DIAGRAM" ]]; then
+  echo "mermaid.sh [-wh] [-f format] diagram"
+  echo
+  echo "See https://mermaid-js.github.io for syntax."
+  exit 0
+fi
+
+DIAGRAM=$(readlink -f $DIAGRAM)
+DIRNAME=$(dirname ${DIAGRAM})
+BASENAME=$(basename -- "${DIAGRAM}")
+RESULT="${BASENAME%.*}.${FORMAT}"
+
+# docker run -it minlag/mermaid-cli
+
+if [[ $WATCH == 'true' ]]; then
+  echo "(Re-)generating $DIRNAME/$RESULT"
+  ls $DIAGRAM | entr bash -c "docker run -u $UID -it -v ${DIRNAME}:/data minlag/mermaid-cli:${MERMAID_VERSION} -i /data/${BASENAME} --output /data/${RESULT}"
+else
+  echo "Generating $DIRNAME/$RESULT"
+  docker run -u $UID -it -v ${DIRNAME}:/data minlag/mermaid-cli:${MERMAID_VERSION} -i /data/${BASENAME} --output /data/${RESULT}
+fi
